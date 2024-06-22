38b90952d167995233cf6f80e0173c686532882c
Rohan1279 <muniruddinrohan@gmail.com>
2024-06-22 20:56:13 +0600

    added state update for height adjustment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,6 @@ export default function Home() {
         backgroundImage: "url('/neon_bubbles_2.jpg')",
         backgroundSize: "cover", // Cover the entire page
         backgroundPosition: "center", // Center the background image
-        zIndex: -1, // Behind the content
       }}
     >
       <div
@@ -24,10 +23,9 @@ export default function Home() {
         <div></div>
         <div></div>
       </div>
-      {/* <ListGradient /> */}
 
       <div
-        className="w-96 h-[484px] rounded-2xl shadow-xl relative flex justify-center subpixel-antialiased z-50 hover:scale-150 transition-all"
+        className="w-96 h-[484px] rounded-2xl shadow-xl relative flex justify-center subpixel-antialiased z-50 hover:scale-150 transition-all  duration-500"
         style={{
           backgroundImage: "url('https://picsum.photos/800/800')",
           backgroundSize: "cover", // Cover the entire page
@@ -38,7 +36,7 @@ export default function Home() {
         <h1 className="absolute -bottom-12 font-semibold text-[26px] tracking-[-1px text-balance"></h1>
       </div>
       <div
-        className="w-96 h-[484px] rounded-2xl shadow-xl  relative flex justify-center subpixel-antialiased z-20 hover:scale-150 transition-all"
+        className="w-96 h-[484px] rounded-2xl shadow-xl  relative flex justify-center subpixel-antialiased z-20 hover:scale-150 transition-all  duration-500"
         style={{
           backgroundImage: "url('https://picsum.photos/800/800')",
           backgroundSize: "cover", // Cover the entire page
@@ -50,6 +48,7 @@ export default function Home() {
         </h1>
         <div className="w-full h-full backdrop-blur-[32px] rounded-2xl"></div>
       </div>
+
       <CardGradient />
     </main>
   );
--- src/components/CardGradient/CardGradient.tsx
+++ src/components/CardGradient/CardGradient.tsx
@@ -1,9 +1,12 @@
+"use client";
 import styles from "./cardGradient.module.css";
 import styles2 from "../ListGradient/gradient.module.css";
+import { useState } from "react";
 const CardGradient = () => {
+  const [blurheight, setBlurheight] = useState("100%");
   return (
     <div
-      className="w-96 h-[484px]  rounded-2xl flex justify-center items-end shadow-xl  relative z-20 group hover:scale-150 transition-all"
+      className="w-96 h-[484px]  rounded-2xl flex justify-center items-end shadow-xl  relative z-20 "
       style={{
         backgroundImage: "url('https://picsum.photos/800/800')",
         backgroundSize: "cover", // Cover the entire page
@@ -11,15 +14,79 @@ const CardGradient = () => {
         // zIndex: -1, // Behind the content
       }}
     >
-      <h1 className="absolute -bottom-10 z-20 font-semibold text-[26px] tracking-[-1px text-balance text-white">
-        Gradient blur
-      </h1>
+      <div className="absolute -bottom-20 z-20 text-white">
+        <h1 className="font-semibold text-[26px] tracking-[-1px] text-balance  text-center">
+          Gradient blur
+        </h1>
+        <div className="w-full flex gap-x-6">
+          <span
+            className={`cursor-pointer rounded-sm bg-white/60 w-8 text-center text-sm ${
+              blurheight === "16.66666666666667%"
+                ? "bg-gray-700 text-white"
+                : "text-black"
+            }`}
+            onClick={() => setBlurheight("16.66666666666667%")}
+          >
+            1/6
+          </span>
+          <span
+            className={`cursor-pointer rounded-sm bg-white/60 w-8 text-center text-sm ${
+              blurheight === "33.33333333333334%"
+                ? "bg-gray-700 text-white"
+                : "text-black"
+            }`}
+            onClick={() => setBlurheight("33.33333333333334%")}
+          >
+            2/6
+          </span>
+          <span
+            className={`cursor-pointer rounded-sm bg-white/60 w-8 text-center text-sm ${
+              blurheight === "50%" ? "bg-gray-700 text-white" : "text-black"
+            }`}
+            onClick={() => setBlurheight("50%")}
+          >
+            3/6
+          </span>
+          <span
+            className={`cursor-pointer rounded-sm bg-white/60 w-8 text-center text-sm ${
+              blurheight === "66.66666666666667%"
+                ? "bg-gray-700 text-white"
+                : "text-black"
+            }`}
+            onClick={() => setBlurheight("66.66666666666667%")}
+          >
+            4/6
+          </span>
+          <span
+            className={`cursor-pointer rounded-sm bg-white/60 w-8 text-center text-sm ${
+              blurheight === "83.33333333333334%"
+                ? "bg-gray-700 text-white"
+                : "text-black"
+            }`}
+            onClick={() => setBlurheight("83.33333333333334%")}
+          >
+            5/6
+          </span>
+          <span
+            className={`cursor-pointer rounded-sm bg-white/60 w-8 text-center text-sm ${
+              blurheight === "100%" ? "bg-gray-700 text-white" : "text-black"
+            }`}
+            onClick={() => setBlurheight("100%")}
+          >
+            full
+          </span>
+        </div>
+      </div>
 
       {/* <div
         className={`absolute w-full h-1/2 backdrop-blur-sm ${styles.mask} flex justify-center items-center `}
       ></div> */}
       <div
-        className={`absolute w-full h-full rounded-2xl overflow-hidden ${styles2.gradientBlur} z-10 `}
+        className={`absolute w-full rounded-2xl overflow-hidden ${styles2.gradientBlur} z-10 `}
+        style={{
+          height: blurheight,
+          transition: "height 0.5s",
+        }}
       >
         <div></div>
         <div></div>
